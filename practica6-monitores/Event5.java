bbcea51fe4c0e0cd6ef6dfdb494ce33c3e63616f
Paula Carrano <65313769+paula-carrano@users.noreply.github.com>
2025-10-04 11:41:10 -0300

    Actualizar Event5.java

--- practica6-monitores/Event5.java
+++ practica6-monitores/Event5.java
@@ -1,21 +1,15 @@
 public class Event5 {
     private int version = 0; // version del evento
-    private int suscriptoresEsperando = 0; // numero de hilos esperando al evento
 
     public synchronized void suscribe() throws InterruptedException {
         int miVersion = version;
-        suscriptoresEsperando++;
         while (version == miVersion) {
-            wait();
         }
-        suscriptoresEsperando--;
     }
 
     public synchronized void publish() {
-        if (suscriptoresEsperando > 0) {
             version++;
             notifyAll();
-        }
     }
 
     public synchronized int getVersion() {
